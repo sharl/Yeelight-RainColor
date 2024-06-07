1c645150ff432e4dcf6a487bab00b00068686afd
Sharl Morlaroll <sharl@hauN.org>
2024-06-07 19:36:06 +0900

    initial release

---
+++ Yeelight-RainColor.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import io
+import threading
+
+import schedule
+from pystray import Icon, Menu, MenuItem
+from PIL import Image, ImageDraw
+import requests
+from bs4 import BeautifulSoup
+from yeelight import Bulb
+
+NAME = 'Yeelight Rain Color'
+
+INTERVAL = 5 * 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class taskTray:
+    def __init__(self):
+        self.running = False
+        self.base = []
+
+        self.readConf()
+
+        item = []
+        item.append(MenuItem('Check', self.doTask))
+        item.append(MenuItem('Reload', self.readConf))
+        item.append(MenuItem('Exit', self.stopApp))
+        menu = Menu(*item)
+
+        # 初期アイコン
+        self.image = Image.new('RGB', (32, 32), WHITE)
+        self.draw = ImageDraw.Draw(self.image)
+        self.app = Icon(name=NAME, title=NAME, icon=self.image, menu=menu)
+
+        self.doTask()
+
+    def readConf(self):
+        home = os.environ.get('HOME', '.')
+        with open(f'{home}/.location') as fd:
+            self.base = fd.read().strip().split('?')
+
+        self.bulb = Bulb('192.168.0.220')
+
+    def doTask(self):
+        r, g, b = self.getRGB()
+        THRESHOLD = 240
+        print(r, g, b)
+
+        if r >= THRESHOLD and g >= THRESHOLD and b >= THRESHOLD:
+            self.bulb.turn_off()
+            self.draw.rectangle((0, 0, 31, 31), fill=BLACK, outline=WHITE)
+        else:
+            self.draw.rectangle((0, 0, 31, 31), fill=(r, g, b), outline=WHITE)
+            self.bulb.turn_on()
+            self.bulb.set_rgb(r, g, b)
+
+        self.app.title = NAME + f' - {r} {g} {b}'
+        self.app.icon = self.image
+        self.app.update_menu()
+
+    def stopApp(self):
+        self.running = False
+        self.app.stop()
+
+    def runSchedule(self):
+        schedule.every(INTERVAL).seconds.do(self.doTask)
+
+        while self.running:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def runApp(self):
+        self.running = True
+
+        task_thread = threading.Thread(target=self.runSchedule)
+        task_thread.start()
+
+        self.app.run()
+
+    def getRGB(self):
+        base_url = f'{self.base[0]}?{self.base[1]}'
+        r = requests.get(base_url)
+        if r and r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            og_images = soup.find_all('meta', property='og:image')
+            if len(og_images) == 0:
+                return BLACK
+            img_url = og_images[0].get('content').replace('1200x630', '1x1')
+
+            r = requests.get(img_url)
+            if r and r.status_code == 200:
+                image = Image.open(io.BytesIO(r.content)).convert('RGB')
+                return image.getpixel((0, 0))
+
+        return BLACK
+
+
+if __name__ == '__main__':
+    taskTray().runApp()
